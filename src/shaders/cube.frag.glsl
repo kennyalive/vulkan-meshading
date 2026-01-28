523cd05f255bd964be7aaae88ff16feec754f2a1
Artem Kharytoniuk <artemalive@gmail.com>
2026-01-28 22:45:03 +0100

    Fix validation error about  variable decoration mismatch
     VUID-RuntimeSpirv-OpVariable-08746

--- src/shaders/cube.frag.glsl
+++ src/shaders/cube.frag.glsl
@@ -1,6 +1,7 @@
 #version 460
+#extension GL_EXT_mesh_shader : enable
 
-layout(location=0) flat in vec3 frag_color;
+layout(location=0) flat in perprimitiveEXT vec3 frag_color;
 layout(location = 0) out vec4 color_attachment0;
 
 void main() {
